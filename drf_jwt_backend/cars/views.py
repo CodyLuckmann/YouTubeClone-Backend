66622bdd99377bce8480b12bf5a9c15e55c1662f
CodyLuckmann <cluckmann@live.com>
2022-01-18 15:22:04 -0600

    added GET to accept get calls

--- drf_jwt_backend/cars/views.py
+++ drf_jwt_backend/cars/views.py
@@ -16,7 +16,7 @@ def get_all_cars(request):
     return Response(serializer.data)     
     
     
-@api_view(['POST'])  
+@api_view(['POST', 'GET'])  
 @permission_classes([IsAuthenticated])
 def user_cars(request):
     if request.method == 'POST':
@@ -25,4 +25,8 @@ def user_cars(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method =='GET':
+        cars = Car.objects.filter(user_id=request.user.id)
+        serializer =CarSerializer(cars, many=True)
+        return Response(serializer.data)
     
