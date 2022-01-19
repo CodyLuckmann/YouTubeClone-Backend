36c728c218c1b0a076395b92e575c99a22317635
CodyLuckmann <cluckmann@live.com>
2022-01-19 14:42:59 -0600

    added new paths

--- drf_jwt_backend/comments/urls.py
+++ drf_jwt_backend/comments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from comments import views
-
 urlpatterns = [
-    path('all/', views.get_all_comments),
+    path('<str:video_id>/', views.get_comments_by_Id),
     path('', views.user_comments),
-    
+    path('reply/', views.reply_to_comments),
+    path('replies/<int:comment_id>/', views.get_replies),
+    path('update/<int:comment_id>', views.update_comment),
+    path('delete/<int:comment_id>', views.delete_comment)
 ]
