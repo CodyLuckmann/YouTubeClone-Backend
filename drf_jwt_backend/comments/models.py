ffdff47d2c244fd1b658b1282c6082f8ad61015d
CodyLuckmann <cluckmann@live.com>
2022-01-19 10:29:05 -0600

    made changes to models

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Comments(models.Model):
+class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=100)
     text = models.CharField(max_length=100)
@@ -10,5 +10,5 @@ class Comments(models.Model):
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
