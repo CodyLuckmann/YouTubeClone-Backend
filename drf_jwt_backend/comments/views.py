d9dfef54425628c03735ee59c1a05fd8c74c8f9b
CodyLuckmann <cluckmann@live.com>
2022-01-19 10:29:57 -0600

    started adding views

--- drf_jwt_backend/comments/views.py
+++ drf_jwt_backend/comments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comment           
+from .serializer import CommentSerializer
+from .models import Reply
+from .serializer import ReplySerializer
+from django.contrib.auth.models import User
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments()
