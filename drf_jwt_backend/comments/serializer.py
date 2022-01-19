9acf5439db8d603b77b36b21f5a420cef16bfc27
CodyLuckmann <cluckmann@live.com>
2022-01-19 10:29:28 -0600

    created serializers for each class

---
+++ drf_jwt_backend/comments/serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Comment
+from .models import Reply
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+    class Meta:
+        model = Comment
+        fields = ['user', 'video_id', 'text', 'likes', 'dislikes']
+        
+class ReplySerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+    class Meta:
+        model = Reply
+        fields = ['user', 'comment', 'text']
