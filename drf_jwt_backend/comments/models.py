8e09e9840088c53ffa9035a1b6c310dee2e9f350
CodyLuckmann <cluckmann@live.com>
2022-01-19 09:08:37 -0600

    fixed an import and adjusted text length to 100

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from authentication.views import User
+from django.contrib.auth.models import User
 
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video_id = models.CharField(max_length=50)
-    text = models.CharField(max_length=50)
+    video_id = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=50)
+    text = models.CharField(max_length=100)
